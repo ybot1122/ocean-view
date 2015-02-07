bb2d7ecc1d2e4c9e9acc93e18abd43ad286b3d58
ybot1122 <ybot1122@gmail.com>
2015-02-07 12:23:42 -0800

    added lib directory

---
+++ public/lib/work.js
@@ -0,0 +1,13 @@
+/*
+
+a) consume APIs and receive
+  - list of buildings that are eligible for X subsidy programs
+  - raw lower-income data for United States
+
+b) 
+
+*/
+
+<script>
+  
+</script>
