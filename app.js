fdcbe296f17c5baa9dfcb7adf2301b77e403876d
ybot1122 <ybot1122@gmail.com>
2015-02-07 11:59:51 -0800

    wrapper for api call

--- app.js
+++ app.js
@@ -16,6 +16,39 @@ app.use(express.static('public'));
 app.use(express.static('public/style'));
 app.use(express.static('public/img'));
 
+// query the multi-family property api
+function api_query(src, params, callback) {
+  var url = '';
+  if (src === 1) {
+    url = 'http://services.arcgis.com/VTyQ9soqVukalItT/arcgis/rest/services/MultiFamilyProperties/FeatureServer/0/query?';
+  }
+  params['f'] = 'geojson';
+  var param_string = JSON.stringify(params);
+  var options = {
+    host: url,
+    path: param_string
+  };
+  http.request(options, function(response) {
+    var str = ''
+    response.on('data', function (chunk) {
+      str += chunk;
+    });
+
+    response.on('end', function () {
+      callback(str);
+    });
+  });
+}
+
+app.route('/events')
+  .all(function(req, res, next) {
+    // runs for all HTTP verbs first
+    // think of it as route specific middleware!
+  })
+  .get(function(req, res, next) {
+    // get request
+  });
+
 // Server start
 var server = app.listen(port, function () {
   var host = server.address().address;
